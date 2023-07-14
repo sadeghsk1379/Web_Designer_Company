daa929c65ef1907279164c2c939df15d5f8cdbb5
sadegh_sargazi <138787095+sadeghsk1379@users.noreply.github.com>
2023-07-14 13:09:22 +0330

    person.py

---
+++ person.py
@@ -0,0 +1,11 @@
+class Person:
+    
+    def __init__(self,First_Name,Last_Name,Age,Address,City) -> None:
+        self.First_Name=First_Name
+        self.Last_Name=Last_Name
+        self.Age=Age
+        self.Address=Address
+        self.City=City
+        
+        pass
+    pass
